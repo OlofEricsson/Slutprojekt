fd58f0e8b60e98f9bfc34fbbcb9e4c6bf1cb66a1
Olof Ericsson <olof.ericsson@elev.ga.ntig.se>
2025-04-01 20:46:35 +0200

    fixade hambmenu till produktsida och snyggade till landing sidan

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-      const menuButton = document.querySelector(".menu-button");
-      const menu = document.querySelector(".menu");
-      const fadeLayer = document.querySelector(".fade-layer");
-  
-      // Visa menyn och fade-layer
-      menuButton.addEventListener("click", () => {
-          menu.classList.add("show");
-          fadeLayer.classList.add("visible");
-      });
-  
-      // Dölj menyn och fade-layer
-      fadeLayer.addEventListener("click", () => {
-          menu.classList.remove("show");
-          fadeLayer.classList.remove("visible");
+    const menuButton = document.querySelector(".menu-button");
+    const menu = document.querySelector(".menu");
+    const fadeLayer = document.querySelector(".fade-layer");
 
-     
-      });
+    menuButton.addEventListener("click", () => {
+        menu.classList.add("show");
+        fadeLayer.classList.add("visible");
+    });
 
-  });
+    fadeLayer.addEventListener("click", () => {
+        menu.classList.remove("show");
+        fadeLayer.classList.remove("visible");
+
+    
+    });
+
+});
